637d86e25623fa21d9212f126dbbb6a5c5a2a334
Joshua Mutiso <joshuamutiso455@gmail.com>
2025-07-25 13:17:42 +0300

    01_01-first

--- CH01/01_01/01_01-first1.c
+++ CH01/01_01/01_01-first1.c
@@ -1,2 +1,10 @@
+#include <stdio.h>
 
-
+/**
+ *
+ * @return "Hello, world!"
+ */
+int main() {
+    puts("Hello, world!");
+    return 0;
+}
--- CH01/01_02/01_02-challenge1.c
+++ CH01/01_02/01_02-challenge1.c
@@ -1,2 +1,6 @@
+#include <stdio.h>
 
-
+int main(){
+    puts("I am a C programmer");
+    return 0;
+}
